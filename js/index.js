531bbba123362bc16e33a905274e4299dc6feed4
dasalos <dasalos@hotmail.com>
2023-06-08 09:35:02 -0500

    Respaldo1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,5 @@
-function a (){
-    x=10
-    alert(x);
-}
-a();
+const nav = document.querySelector('nav');
 
-function b(){
-    y=12;
-    alert(x);
-    alert(x+y);
-}
-
-b();
-function PRUEBAS() {
- abc=123;
- bca = 321;
- alert(bca);
-alert(abc);
-}
+window.addEventListener('scroll', function(){
+    nav.classList.toggle('active', this.window.scrollY >0)
+})
