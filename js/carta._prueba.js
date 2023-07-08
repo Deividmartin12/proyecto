bbfc140e0e41e92d93dd99b7a059cb440d591e22
David <caballerovasquezdavidantonio@gmail.com>
2023-07-08 04:32:33 -0500

    Prueba_Caballero

---
+++ js/carta._prueba.js
@@ -0,0 +1,5 @@
+function generar_subcategorias(){
+    /*document.getElementById("barra_sub").innerHTML('<ul class="nav nav-justified"> <li class="nav-item"><a class="nav-link" href="#">Arroces y carnes</a> </li> <li class="nav-item"><a class="nav-link" href="#">Pastas</a> </li> <li class="nav-item"><a class="nav-link" href="#">Sopas</a></li><li class="nav-item"><a class="nav-link" href="#">Pescado y mariscos</a></li></ul>');
+*/
+
+}
