f21d38b3a0ade5c6a575a6f69ed1f7913d12d44e
Deividmartin12 <131992849+Deividmartin12@users.noreply.github.com>
2023-07-12 23:25:52 -0500

    cornejo

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,8 @@
 const ordenar = document.querySelectorAll(".ordenar_carrito");
 
+document.addEventListener("DOMContentLoaded", () => {
+  show_carrito();
+});
 ordenar.forEach((boton) => {
   boton.addEventListener("click", () => {
     const title = boton.parentElement.querySelector(".card-title").textContent;
@@ -20,3 +23,23 @@ ordenar.forEach((boton) => {
     };
   });
 });
+
+function show_carrito() {
+  const info_carrito = document.querySelector(".info_carrito");
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const obj = JSON.parse(localStorage.getItem(key));
+    console.log(obj);
+
+    const { title, src, precio } = obj;
+
+    info_carrito.innerHTML += `<ul class="navbar-nav justify-content-end flex-grow-1 pe-3"> 
+          <div class="d-flex justify-content-center gap-2">
+            <h2 class="fs-5">${title}</h2>
+            <h1 class="fs-5">Precio: ${precio}</h1>
+            
+          </div>
+    </ul>`;
+  }
+}
